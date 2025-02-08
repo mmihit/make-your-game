3a817bc0ad05b32a91bd2164aad30a99f50273a0
mmihit <med86004@gmail.com>
2025-02-08 15:00:28 +0100

    handle moving of ball

--- script.js
+++ script.js
@@ -1,42 +1,131 @@
-var paddle = document.querySelector(".paddle")
+const paddleElement = document.querySelector(".paddle")
+const ballElement = document.querySelector(".ball")
 
+const gameState = {
+    start:false,
+    ballMoving:false
+}
 
-let TranslateXOfPaddle=getTheTranslateXPerecntageOfPaddle(paddle.style.transform)
+const keys = {
+    ArrowLeft: false,
+    ArrowRight: false
+}
 
-// document.addEventListener('keyup', (e)=>{
-//     if(e.keyCode===39){
-//         console.log("left right click")
-//     }
-//     if (e.keyCode===37) {
-//         console.log("click")
-//     }
-// })
+const paddle = {
+    speed: 15,
+    boundaries: {
+        left: -350,
+        right: 250
+    },
+    positionXOfPaddle: -50,
+}
+
+const ball = {
+    matchPaddleSpeed: 15,
+    speed: 5,
+    position: [341, 502],
+    direction: [1, -1],
+    boundaries: {
+        left: 0,
+        right: 680,
+        top: 0,
+        bottom: 530
+    }
+}
 
 document.addEventListener('keydown', (e) => {
-    paddle = document.querySelector(".paddle")
-    let arg=0
-    if (e.keyCode === 39 && TranslateXOfPaddle!=250) {
-        arg=20
-        updateTheTranslateXPercentageOfPaddle(TranslateXOfPaddle, arg)
+    if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+        keys[e.key] = true
     }
-    if (e.keyCode === 37 && TranslateXOfPaddle!=-350) {
-        arg=-20
-        updateTheTranslateXPercentageOfPaddle(TranslateXOfPaddle, arg)
-    }
-    
 })
 
-function getTheTranslateXPerecntageOfPaddle(transform) {
-    const translateMatch = transform.match(/translateX\((-?\d+\.?\d*)%\)/);
+document.addEventListener('keyup', (e) => {
+    if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+        keys[e.key] = false
+    }
+})
+
+document.addEventListener('keyup', (e) => {
+    if (e.key === ' ') {
+        if (!gameState.start) {
+            gameState.start = true
+        }
+    }
+})
+
+function gameLoop() {
+
+    if (gameState.start) {
+        document.addEventListener('keyup', (e) => {
+            if (e.key === ' ') {
+                if (!gameState.ballMoving) {
+                    gameState.ballMoving = true
+                }
+            }
+        })
+        if (keys.ArrowRight) {
+            paddle.positionXOfPaddle = movePaddleRight()
+        }
+        if (keys.ArrowLeft) {
+            paddle.positionXOfPaddle = movePaddleLeft()
+        }
+    }
+
+    
+
+
+    updatePaddlePosition(paddle.positionXOfPaddle)
+
+
+    if (gameState.ballMoving) {
+        moveBall()
+        updateBallPosition()
+    }
+
+
+
+
+    requestAnimationFrame(gameLoop)
+}
+
+function getPaddlePosition(transform) {
+    const translateMatch = transform.match(/translateX\((-?\d+\.?\d*)px\)/);
     if (translateMatch) {
         return parseFloat(translateMatch[1]);
     }
     return 0
 }
 
-function updateTheTranslateXPercentageOfPaddle(transform, flag) {
-    paddle.style.transform=`TranslateX(${transform+flag}%)`
-    TranslateXOfPaddle=transform+flag
-    console.log(transform+flag)
-    console.log(paddle.style.transform)
+function movePaddleRight() {
+    return paddle.positionXOfPaddle < paddle.boundaries.right ? paddle.positionXOfPaddle + paddle.speed : paddle.positionXOfPaddle
 }
+
+function movePaddleLeft() {
+    return paddle.positionXOfPaddle > paddle.boundaries.left ? paddle.positionXOfPaddle - paddle.speed : paddle.positionXOfPaddle
+}
+
+function updatePaddlePosition(transform) {
+    paddleElement.style.transform = `translateX(${transform}px)`
+}
+
+function moveBall() {
+    ball.position[0] += ball.speed * ball.direction[0]
+    ball.position[1] += ball.speed * ball.direction[1]
+
+    if (ball.position[0] <= ball.boundaries.left || ball.position[0] >= ball.boundaries.right) {
+        ball.direction[0] *= -1
+    }
+    if (ball.position[1] <= ball.boundaries.top || ball.position[1] >= ball.boundaries.bottom) {
+        ball.direction[1] *= -1
+    }
+}
+
+function updateBallPosition() {
+    ballElement.style.transform = `translate(${ball.position[0]}px, ${ball.position[1]}px)`
+}
+
+
+
+requestAnimationFrame(gameLoop)
+
+
